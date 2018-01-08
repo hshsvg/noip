f9c1d9b7dd44d5bd837fcd22a7728d2e9691409e
hshsvg <hshsvg@qq.com>
2018-01-08 23:37:35 +0800

    增加了减去n位数字得到最小数字的程序,重新排版了

--- delnum.cpp
+++ delnum.cpp
@@ -12,17 +12,16 @@ using namespace std;
 
 int main()
 {
-    string s;		// 输入字符串
-    
+	string s;		// 输入字符串
 	int n, k, i;	// 可去掉个数，保留个数，当前查找位
 	int len;	    // 输入字符串长度
 	int left = 0; 	// 范围最左
 	cin >> s;
-    cin >> n;
-    int ans = 0;
-    len = s.length();
+	cin >> n;
+	int ans = 0;
+	len = s.length();
 	k = len - n;      // k即为保留个数，就是原来的个数减去要去掉的个数
-    while (k > 0)
+	while (k > 0)
 	{
 		int dqmin = s[left] - '0';	//当前位最小
 		left++;
